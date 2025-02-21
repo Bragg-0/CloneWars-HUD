d618c254e43c553a9fbf9748c802a7c4804d94f6
Bragg <maxime.brochet.98@gmail.com>
2025-02-21 01:01:13 +0100

    Ajout de nouveaux fichiers d'en-tête pour la gestion du HUD et des macros associées

--- CWHUD/macros.hpp
+++ CWHUD/macros.hpp
@@ -29,21 +29,6 @@
 #define Blue {0, 0, 1, 1}
 #define Gray {0.5, 0.5, 0.5, 1}
 
-#define IDD_MENU 3142100
-
-#define IDC_MENU_TESTHUDFrame 3142101
-#define IDC_MENU_TESTHUDColor 3142102
-#define IDC_MENU_slider_green 3142103
-#define IDC_MENU_slider_red 3142104
-#define IDC_MENU_slider_blue 3142105
-#define IDC_MENU_slider_alpha 3142106
-
-#define IDD_HUD 8842100
-
-#define IDC_HUD_FRAME 8842101
-#define IDC_HUD_COLOR 8842102
-#define IDC_HUD_SUNFILTER 8842103
-
 #define SliderStyle                                                \
     arrowEmpty = "\A3\ui_f\data\GUI\Cfg\Slider\arrowEmpty_ca.paa"; \
     arrowFull = "\A3\ui_f\data\GUI\Cfg\Slider\arrowFull_ca.paa";   \
